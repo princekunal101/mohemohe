0cb441e254267a257e1cf90b7000fa543bef2df5
Prince Kunal <princekunal652@gmail.com>
2024-04-27 00:54:36 +0530

    copyright added

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -23,6 +23,9 @@ export function SiteFooter() {
         <div className="mb-2 flex space-x-2 text-sm text-muted-foreground">
           <a href={siteConfig.links.personalSite} target="_blank">{siteConfig.author}</a>
         </div>
+        <div className="mb-2 flex space-x-2 text-sm text-muted-foreground">
+          <p> &copy; 2024 <a href="https://www.mohemohe.in">{siteConfig.name}</a> | All rights Reserved </p>
+        </div>
       </div>
     </footer>
   )
