e2941eaeb00bc3253cc96a7bcdf54a6a6be7f159
Prince Kunal <princekunal652@gmail.com>
2024-05-12 22:06:26 +0530

    bug fix

--- components/mdx-intro.tsx
+++ components/mdx-intro.tsx
@@ -14,7 +14,7 @@ export function ChapIntro({
   ...props
 }: ChapIntroProps) {
   return (<div className={cn(" my-2 md:-mx-10 md:p-10 md:my-4 md:rounded-md md:bg-gray-100 md:dark:bg-gray-800 ")}><h3 className={cn("my-0")}>In this chapter...</h3>
-    <p className={cn("mb-2 text-sm")}>Here are the topics we'll cover</p>
+    <p className={cn("mb-2 text-sm")}>Here are the topics we&apos;ll cover</p>
     <div className={cn(`my-0 items-start rounded-md border border-inherit bg-background shadow-md p-4 dark:max-w-none ${txCenter ? "text-center" : null}`)}
       {...props}>
       <ul className={cn("my-2 px-0")}>{children}</ul>
