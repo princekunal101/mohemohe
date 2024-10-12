b58c26244a98e3e845ae76ea63ed1a71d8dcdc25
Prince Kunal <princekunal652@gmail.com>
2024-10-12 20:19:02 +0530

    twitter link

--- config/site.ts
+++ config/site.ts
@@ -6,7 +6,7 @@ export const siteConfig = {
   author: "Prince Kunal",
   keywords: ["mohemohe.in", " Data Structure", " Algorithms", " Btech", " DSA", " Tafal", " Theory of Automata", " Java", " C", " C++", " OS", " Operating System", " Mohe Mohe", " CSE", " React", " HTML", " CSS", " PHP", " JavaScript", " Aktu", " Web Development", " Computer Science", " Programming", " Python", " Tutorial", " NextJS", " Mohe Mohe", " Beginners", " Professionals", " Graduation", " Students"],
   links: {
-    twitter: "https://twitter.com/princekunal",
+    x: "https://x.com/mohemoheIN",
     github: "https://github.com/princekunal101/mohemohe",
     personalSite: "https://kunalzone.com",
     linkedin: "https://www.linkedin.com/in/prince-kunal-45b8a4287",
