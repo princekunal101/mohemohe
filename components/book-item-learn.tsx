7147f28e1a128d3aa7afc3a5fe2951728b7f4da2
Prince Kunal <princekunal652@gmail.com>
2024-12-03 01:20:17 +0530

    header book logo rebuild

--- components/book-item-learn.tsx
+++ components/book-item-learn.tsx
@@ -15,17 +15,20 @@ export default function BookItemLearn({ slug, bookName, bookDesc, bookShortDesc 
   return (
     <div className="w-full lg:max-w-3xl shadow flex flex-col md:flex-row items-center justify-center md:justify-left border rounded-xl p-4">
       <div className=" flex justify-center">
-        <div className="w-20 relative flex aspect-[3/4] shrink-0 shadow-inner-bl bg-green-900 overflow-hidden rounded-[3px]">
-          <div className="h-full w-1 bg-gradient-to-r from-black/25 from-30% via-white/75 via-55% to-white/45 to-80%" />
-          <div className="h-full w-0.5 bg-gradient-to-r from-white/45 from-30% to-black/15 " />
-          <div className="h-full w-1 bg-gradient-to-r from-black/15 from-10% via-white/40  to-white/10 to-95%" />
-          <div className="h-full w-2 bg-gradient-to-r from-white/10  to-transparent " />
-          <div className="absolute w-full flex-1 px-2 py-1.5 justify-center text-zinc-100 text-center">
-            <p className=" w-full mb-0.5 font-bold line-clamp-2 text-[0.55rem] opacity-95">{bookName}</p>
-            <p className=" text-[0.35rem]  line-clamp-3 opacity-95">{bookShortDesc}</p>
-          </div>
-          <div className="absolute w-full justify-center flex bottom-1 text-white/95">
-            <Icons.logo className="w-8 h-8" />
+        <div className="w-20 relative flex aspect-[3/4] shrink-0  bg-green-900 shadow-lg shadow-green-800/50 overflow-hidden rounded-[3px]">
+          <div className="flex shadow-inner-bl">
+
+            <div className="h-full w-1 bg-gradient-to-r from-black/25 from-30% via-white/75 via-55% to-white/45 to-80%" />
+            <div className="h-full w-0.5 bg-gradient-to-r from-white/45 from-30% to-black/15 " />
+            <div className="h-full w-1 bg-gradient-to-r from-black/15 from-10% via-white/40  to-white/10 to-95%" />
+            <div className="h-full w-2 bg-gradient-to-r from-white/10  to-transparent " />
+            <div className="absolute w-full flex-1 px-2 py-1.5 justify-center text-zinc-100 text-center">
+              <p className=" w-full mb-0.5 font-bold line-clamp-2 text-[0.55rem] opacity-95">{bookName}</p>
+              <p className=" text-[0.35rem]  line-clamp-3 opacity-95">{bookShortDesc}</p>
+            </div>
+            <div className="absolute w-full justify-center flex bottom-1 text-white/95">
+              <Icons.logo className="w-8 h-8" />
+            </div>
           </div>
         </div>
       </div>
@@ -49,17 +52,19 @@ export function BookMainItemLearn({ slug, bookName, bookDesc, bookShortDesc }: B
     <div className="flex justify-center bg-background pt-20">
       <div className="w-full lg:max-w-3xl shadow flex flex-col items-center justify-center border rounded-xl p-4">
         <div className="w-full flex justify-center -mt-16">
-          <div className="w-40 relative flex aspect-[3/4] shadow-inner-bl bg-green-900 overflow-hidden rounded">
-            <div className="h-full w-2 bg-gradient-to-r from-black/25 from-30% via-white/75 via-55% to-white/45 to-80%" />
-            <div className="h-full w-1 bg-gradient-to-r from-white/45 from-30% to-black/15 " />
-            <div className="h-full w-2 bg-gradient-to-r from-black/15 from-10% via-white/40  to-white/10 to-95%" />
-            <div className="h-full w-4 bg-gradient-to-r from-white/10  to-transparent " />
-            <div className="absolute w-full flex-1 p-3 text-zinc-100 justify-center text-center">
-              <p className=" w-full mb-2 text-balance font-bold opacity-95">{bookName}</p>
-              <p className=" line-clamp-4 text-[0.7rem] opacity-95">{bookShortDesc}</p>
-            </div>
-            <div className="absolute w-full justify-center flex bottom-3 text-white/95">
-              <Icons.logo className="w-14 h-14" />
+          <div className="w-40 relative flex aspect-[3/4] bg-green-900 overflow-hidden shadow-2xl shadow-green-800/60 rounded">
+            <div className=" w-full flex shadow-inner-bl">
+              <div className="h-full w-2 bg-gradient-to-r from-black/25 from-30% via-white/75 via-55% to-white/45 to-80%" />
+              <div className="h-full w-1 bg-gradient-to-r from-white/45 from-30% to-black/15 " />
+              <div className="h-full w-2 bg-gradient-to-r from-black/15 from-10% via-white/40  to-white/10 to-95%" />
+              <div className="h-full w-4 bg-gradient-to-r from-white/10  to-transparent " />
+              <div className="absolute w-full flex-1 p-3 text-zinc-100 justify-center text-center">
+                <p className=" w-full mb-2 text-balance font-bold opacity-95">{bookName}</p>
+                <p className=" line-clamp-4 text-[0.7rem] opacity-95">{bookShortDesc}</p>
+              </div>
+              <div className="absolute w-full justify-center flex bottom-3 text-white/95">
+                <Icons.logo className="w-14 h-14" />
+              </div>
             </div>
           </div>
         </div>
@@ -74,6 +79,28 @@ export function BookMainItemLearn({ slug, bookName, bookDesc, bookShortDesc }: B
             </Link>
           </div>
         </div>
+      </div>
+    </div>
+  )
+}
+
+interface BookItemChapterHeaderProps {
+  bookName: string;
+  bookShortDesc?: string;
+}
+export function BookItemChapterHeader({ bookName, ...props }: BookItemChapterHeaderProps) {
+  return (
+    <div className="w-12 flex relative aspect-[3/4] shadow-inner-bl bg-green-900 overflow-hidden rounded-[2px]">
+      <div className="h-full w-0.5 bg-gradient-to-r from-black/25 from-30% via-white/75 via-55% to-white/45 to-80%" />
+      <div className="h-full w-[1px] bg-gradient-to-r from-white/45 from-30% to-black/15 " />
+      <div className="h-full w-0.5 bg-gradient-to-r from-black/15 from-10% via-white/40  to-white/10 to-95%" />
+      <div className="h-full w-2 bg-gradient-to-r from-white/10  to-transparent " />
+      <div className="absolute w-full flex-1 p-1 text-zinc-100 justify-center text-center">
+        <p className=" w-full mb-2 text-[0.4rem] text-balance font-bold opacity-95">{bookName}</p>
+        {/* <p className=" line-clamp-4 text-[0.7rem] opacity-95">{bookShortDesc}</p> */}
+      </div>
+      <div className="absolute w-full justify-center flex bottom-0 text-white/95">
+        <Icons.logo className="w-8 h-8" />
       </div>
     </div>
   )
